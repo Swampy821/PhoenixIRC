643fd2d8e54ee1de6de254171acd2caa2d25e368
Steve Marsh <Zimdale@gmail.com>
2014-01-24 17:48:44 -0500

    bug fixes in admin

--- plugins/admin.js
+++ plugins/admin.js
@@ -5,28 +5,32 @@
 //MESSAGE EVENT
 exports.message = function(from, to, text, message, bot, config){
     var messageArray = text.split(' ');
+    var channel = message.args[0];
     //Trim Character.
     messageArray[0] = messageArray[0].substring(0,messageArray[0].length-1);
     //Are you talking to the bot?
     if(messageArray[0]==config.botName && messageArray.length>1) {
           //Opper
           if(messageArray[1].toLowerCase()=='op') {
-                bot.send('MODE', config.channels[0], '+o', messageArray[2]);
+             if(messageArray[2]!=config.botName) {
+                if(config.nicks[messageArray[2]]!=undefined) {
+                  bot.send('MODE', channel, '+o', messageArray[2]);
+                }
+              }
+              
           }
           //Deopper
           if(messageArray[1].toLowerCase()=='deop') {
                if(messageArray[2]!=config.botName) {
-                    bot.send('MODE', config.channels[0], '-o', messageArray[2]);
+                 if(config.nicks[messageArray[2]]!=undefined) {
+                    bot.send('MODE', channel, '-o', messageArray[2]);
+                  }
                }
           }
           //Quitter
           if(messageArray[1].toLowerCase()=='die' && config.admins.indexOf(from)>-1) {
                process.exit();
           }
-
-
-
-
     }
 
 
