853977c769b527a5ca2602805760333fdbfcb865
SkySom <skylar.som@gmail.com>
2014-01-25 15:29:22 -0500

    UPdate of Youtube. Still not working.

--- plugins/Youtube.js
+++ plugins/Youtube.js
@@ -2,9 +2,10 @@
  * @Author SkySom
  * @Date 1/24/14
  */
-var http = require('http');
+
 
 exports.message = function(from, to, text, message, bot, config) {
+
     var patt = new RegExp("youtube" , "i");
     var res = patt.test(text);
     if(res) {
@@ -15,14 +16,43 @@ exports.message = function(from, to, text, message, bot, config) {
             query = encodeURIComponent(query);
             var options = {
                 hostname : "http://gdata.youtube.com",
-                path : "/feeds/api/videos?v=2&orderby=relevance&paid-content=false&duration=long&query=" + query
+                path : "/feeds/api/videos?v=2&orderby=relevance&paid-content=false&alt=jsonc&duration=long&query=" + query
             }
+            console.log(options.hostname + options.path);
+            var chk = '';
+            var http = require('http');
+            http.get(options, function(resp){
+                resp.setEncoding('utf8');
+                resp.on('data', function(chunk){
+                    chk+=chunk;
+                });
+                resp.on('end',function() {
+                    try{
+                        js = JSON.parse(chk);
+                        if(js.length>0) {
+                            bot.say(to, from+': '+js[0].text);
+                        }else{
+                            bot.say(to, from+': I do not have a definition for that word');
+                        }
+                    }catch(e) {
+                        bot.say(to, from+': Invalid character');
+                    }
+                });
+                resp.on('err', function(e) {
+                   bot.say(to, e);
+                });
+            });
+
             var req = http.request(options, function(response) {
                 bot.say(to, "Making the request");
             });
 
+            req.on('data', function(chunk) {
+               bot.say(to, "Recieving data");
+            });
+
             req.on('error', function(e) {
-                bot.say(to, 'q=' + query + ' error=' + e);
+                bot.say(to, 'q=' + query + e);
             });
 
             bot.say(to, "Youtubing " + query);
