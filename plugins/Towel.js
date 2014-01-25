7946864d0566652f573a075942546bd87e55b479
Steve Marsh <Zimdale@gmail.com>
2014-01-25 16:24:47 -0500

    Added a Towel plugin

---
+++ plugins/Towel.js
@@ -0,0 +1,6 @@
+//MESSAGE EVENT
+exports.message = function(from, to, text, message, bot, config){
+	if(text.toLowerCase().indexOf('a towel')>-1) {
+		bot.say(to,'http://tiger.towson.edu/~melvid1/towelie%20typing.jpg');
+	}
+}
