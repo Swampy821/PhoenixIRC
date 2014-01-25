7ecc6345d4343b82095f9befd90f32a7c6e3a80a
SkySom <skylar.som@gmail.com>
2014-01-24 21:47:43 -0500

    Added GoodByes, removed unused methods from FuckYeah and Quotes

--- plugins/Quotes.js
+++ plugins/Quotes.js
@@ -7,23 +7,3 @@
 exports.message = function(from, to, text, message, bot, config) {
 
 }
-
-//Join Event
-exports.join = function(channel, nick, message, bot, config) {
-
-}
-
-//Part Event
-exports.part = function(channel, nick, message, bot, config) {
-
-}
-
-//Action Event
-exports.action = function(from, to, message, bot, config) {
-
-}
-
-//Raw Event
-exports.raw = function(message, bot, config){
-
-}
