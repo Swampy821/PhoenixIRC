54b759028048b262889e99704bfaaee294545829
Andy <5thWall@gmail.com>
2014-07-14 20:49:59 -0700

    Better jokes

--- plugins/Jokes.js
+++ plugins/Jokes.js
@@ -1,27 +1,22 @@
 //MESSAGE EVENT
 exports.message = function(from, to, text, message, bot, config){
-	if(config.plugins.jokes && text.toLowerCase().indexOf('lets hear a joke')>-1) {
-			var http = require('http');
-			var options = {
-			  host: 'www.jokels.com',
-			  path: '/random_joke'
-			};
-			var chk = '';
-			http.get(options, function(resp){
-				resp.setEncoding('utf8');
-			  resp.on('data', function(chunk){
-			  	   chk+=chunk;
-			  });
-			  resp.on('end',function() {
-			  		try{
-				  		js = JSON.parse(chk);
-				  			bot.say(to,js.joke.question);
-				  			setTimeout(function() {
-				  				bot.say(to,js.joke.answer);
-				  			},5000);
-			  		}catch(e) {
-			  		}
-			  });
-			});
-	}
-}
+  if(config.plugins.jokes && text.toLowerCase().indexOf('lets hear a joke')>-1) {
+    var request = require('request');
+
+    var options = {
+      url: 'http://reddit.com/r/jokes.json',
+      json: true
+    };
+
+    request(options, function(error, response, body) {
+      if (!error) {
+        var children = body.data.children;
+        var joke = children[Math.floor(Math.random() * children.length)].data;
+
+        var joketext = joke.title.replace(/\.\.\.$/, '') + ' ' + joke.selftext.replace(/^\.\.\.\s*/, '');
+
+        bot.say(to, joketext);
+      }
+    });
+  }
+};
