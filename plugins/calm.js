9f09fc26520f3be736ec1f093995c04f697a41ce
Steve Marsh <Zimdale@gmail.com>
2014-10-14 16:41:30 -0400

    Fixing cases

--- plugins/calm.js
+++ plugins/calm.js
@@ -4,7 +4,7 @@ exports.init = function (bot, config) {
 
 //MESSAGE EVENT
 exports.message = function(from, to, text, message, bot, config){
-	if(config.plugins.calm===true && text.match(/calm down/g) !== null) {
+	if(config.plugins.calm===true && text.toLowerCase().match(/calm down/g) !== null) {
 		var num = Math.floor(Math.random() * 30) + 1;
 		bot.say(to, 'http://calmingmanatee.com/img/manatee'+num+'.jpg');
 	}
