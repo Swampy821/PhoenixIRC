be340a3d4674e59b8a3b8ac59f674915f9f089cc
root <Zimdale@gmail.com>
2014-10-14 10:41:03 -0400

    Began work on translate plugin

---
+++ plugins/translate.js
@@ -0,0 +1,49 @@
+var request = require('request');
+function translate() {}
+translate.prototype.process = function(text, language, callback) {
+   request('http://mymemory.translated.net/api/get?q='+text+'&langpair='+language+'|en', function(error, response, body) {
+      body = JSON.parse(body);  
+      if(body.responseData !== undefined) {
+        callback(body.responseData.translatedText);
+      }else{
+        callback('I do not have a translation for that!');
+      }
+   });
+};
+
+var t = new translate();
+
+// Plugin initialization.
+exports.init = function (bot, config) {
+}
+
+//MESSAGE EVENT
+exports.message = function(from, to, text, message, bot, config){
+  if(text.split(' ')[0] == '!translate') {
+     
+     var transtext = text.split(' ').splice(1,text.length).join(' ');
+     t.process(transtext, 'it', function(newText) {
+         bot.say(to, from+': ' + newText);
+     });
+  }
+}
+
+//JOIN EVENT
+exports.join = function(channel, nick, message, bot, config){
+
+}
+
+//PART EVENT
+exports.part = function(channel, nick, message, bot, config){
+
+}
+
+//PART EVENT
+exports.raw = function(message, bot, config){
+
+}
+
+//ACTION EVENT
+exports.action = function(from, to, message, bot, config){
+
+}
