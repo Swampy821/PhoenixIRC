dd25f41a29921b1de7117d90a7a57b368f533f50
Steve Marsh <Zimdale@gmail.com>
2014-10-31 12:15:39 -0400

    Fixed youtube plugin

--- plugins/Youtube.js
+++ plugins/Youtube.js
@@ -1,51 +1,15 @@
-/**
- * @Author SkySom
- * @Date 1/24/14
- */
-
+var jsdom = require('jsdom');
 
 exports.message = function(from, to, text, message, bot, config) {
-    if(config.plugins.youtube) {
-        var patt = new RegExp("youtube" , "i");
-        var res = patt.test(text);
-        if(res) {
-            var locationOfYoutube = text.toLowerCase().indexOf("youtube");
-            if(locationOfYoutube != -1) {
-                var query = text.substr(locationOfYoutube + "youtube".length);
-                query = query.trim();
-                query = encodeURIComponent(query);
-                var options = {
-                    hostname : "gdata.youtube.com",
-                    path : "/feeds/api/videos?v=2&orderby=relevance&paid-content=false&alt=jsonc&duration=long&query=" + query
-                }
-                console.log(options.hostname + options.path);
-                var chk = '';
-                var http = require('http');
-                http.get(options, function(resp){
-                    resp.setEncoding('utf8');
-                    resp.on('data', function(chunk){
-                        chk+=chunk;
-                    });
-                    resp.on('end',function() {
-                        try{
-                            js = JSON.parse(chk);
-                            console.log(js.length);
-                            if(js.length>0) {
-                                bot.say(to, "The length of js is " + js.length);
-                            }else{
-                                bot.say(to, "There is nothing");
-                            }
-                        }catch(e) {
-                            bot.say(to, "Error " + e);
-                        }
-                    });
-                    resp.on('err', function(e) {
-                       bot.say(to, e);
-                    });
-                });
-
-                bot.say(to, "Youtubing " + query);
+    if(config.plugins.youtube && text.indexOf('youtube.com')>-1) {
+        jsdom.env(
+          text,
+          ["http://code.jquery.com/jquery.js"],
+          function (errors, window) {
+            if(window.$("#eow-title").text().length>5) {
+                bot.say(to,window.$("#eow-title").text().replace(/\n|\r/g,''));
             }
-        }
+          }
+        );       
     }
 }
