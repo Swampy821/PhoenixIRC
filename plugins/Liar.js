eb0d2512bf25c085ed08583c5992dcab2dd5bfe8
Steve Marsh <Zimdale@gmail.com>
2014-10-31 15:18:15 -0400

    Fixed Liar plugin

--- plugins/Liar.js
+++ plugins/Liar.js
@@ -4,7 +4,7 @@
 
 exports.message = function(from, to, text, message, bot, config) {
 	if(config.plugins.liar) {
-	    var patt = new RegExp("(^|\s)liar(\s|$)|(^|\s)lie(\s|$)|(^|\s)lies(\s|$)" , "i");
+	    var patt = new RegExp("(?:^|\\b)(lie|liar|lies)(?=\\b|$)", "gi");
 	    var res = patt.test(text);
 	    if(res) {
 	        bot.say(to, "http://i.imgur.com/XvUsaD9.jpg");
