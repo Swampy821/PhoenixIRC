4765568fea978df5ced7fb56d47440a6cd178ba6
Steve Marsh <Zimdale@gmail.com>
2014-10-31 16:16:09 -0400

    Added catfacts

--- plugins/Liar.js
+++ plugins/Liar.js
@@ -4,7 +4,7 @@
 
 exports.message = function(from, to, text, message, bot, config) {
 	if(config.plugins.liar) {
-	    var patt = new RegExp("(?:^|\\b)(lie|liar|lies|lying)(?=\\b|$)", "gi");
+	    var patt = new RegExp("(?:^|\\b)(lie|liar|lies|lying|liars)(?=\\b|$)", "gi");
 	    var res = patt.test(text);
 	    if(res) {
 	        bot.say(to, "http://i.imgur.com/XvUsaD9.jpg");
