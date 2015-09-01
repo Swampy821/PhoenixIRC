7e8cac317aec1b9bebe559e174358dbc3356389d
Steve Marsh <smarsh@greenlancer.com>
2015-08-31 20:34:22 -0400

    Added some stuff to finance

--- plugins/finance.js
+++ plugins/finance.js
@@ -1,4 +1,5 @@
 var request = require("request");
+var old = {};
 //MESSAGE EVENT
 exports.message = function(from, to, text, message, bot, config){
 	 if(config.plugins.finance) {
@@ -13,9 +14,28 @@ exports.message = function(from, to, text, message, bot, config){
 					try{
 						var data = JSON.parse(body);
 						var quote = data.list.resources[0].resource.fields;
-						bot.say(to, from + ": " + quote.symbol + " - " + quote.name + " - $" + quote.price + "");
+						var change = 0;
+						var changeText = "";
+						if(!old[quote.symbol]) {
+							old[quote.symbol] = {
+								price:quote.price
+							};
+						}else{
+							change = quote.price - old[quote.symbol].price;
+
+							if(change>=0) {
+								if(change>0) {
+									change = Math.max( Math.round((change / old[quote.symbol].price) * 100)).toFixed(2);
+								}
+								changeText = " (+" + change + "%)";
+							}else if(change < 0) {
+								change = Math.max( Math.round((change / old[quote.symbol].price) * 100)).toFixed(2);								
+								changeText = " (" + change + "%)";
+							}
+						}
+						bot.say(to, from + ": " + quote.symbol + " - " + quote.name + " - $" + quote.price + "" + changeText);
 					}catch(e) {
-						bot.say(to, from + ": Could not complete lookup.");
+						bot.say(to, from + ": Could not find that symbol.");
 					}
 				});
 			}
